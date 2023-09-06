376c8938fcc885e159075c1318bff064e57b235d
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2023-09-06 08:20:06 +0100

    vis code

---
+++ visualiser.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+df=pd.read_csv("D:/Documents/AI/ML challenges/2023S1/movementSensorData.csv")
+
+
+print(df.keys())
+x=df.loc[df['activity'] == 1, 'lw_x']
+y=df.loc[df['activity'] == 4, 'lw_x']
+plt.plot(x,label="Walking x-axis")
+plt.plot(y,label="Driving x-axis")
+plt.legend(loc="upper right")
+plt.xlabel("Time step")
+plt.ylabel("Value")
+plt.show()
+
+x=df.loc[df['activity'] == 1, 'lw_y']
+y=df.loc[df['activity'] == 4, 'lw_y']
+plt.plot(x,label="Walking y-axis")
+plt.plot(y,label="Driving y-axis")
+plt.legend(loc="upper right")
+plt.xlabel("Time step")
+plt.ylabel("Value")
+plt.show()
+
+x=df.loc[df['activity'] == 1, 'lw_z']
+y=df.loc[df['activity'] == 4, 'lw_z']
+plt.plot(x,label="Walking z-axis")
+plt.plot(y,label="Driving z-axis")
+plt.legend(loc="upper right")
+plt.xlabel("Time step")
+plt.ylabel("Value")
+plt.show()
